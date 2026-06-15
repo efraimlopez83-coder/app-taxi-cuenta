bba66115e8315f5f14466707a6d71d44f00edd0e
efraimlopez83-coder <efraimlopez83@gmail.com>
2026-06-15 18:23:13 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'cuentas-taxi-v9-pwa';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
